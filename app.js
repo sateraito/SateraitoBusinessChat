96c183927c3318ca66bacc9ecd4f0aa20abe73d5
Nguyen Duc Huu Tri <huutri1983@gmail.com>
2016-10-11 10:50:16 +0700

    commit

--- app.js
+++ app.js
@@ -55,26 +55,28 @@ app.use(session(
 
 
 
-var pg = require('pg');
-
-app.get('/', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/db', {results: result.rows} ); }
-        });
-    });
-});
-
+// var pg = require('pg');
 //
-//
-// app.get("/", function (req, res) {
-//
-//     res.render("index", {})
+// app.get('/', function (request, response) {
+//     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//         client.query('SELECT * FROM test_table', function(err, result) {
+//             done();
+//             if (err)
+//             { console.error(err); response.send("Error " + err); }
+//             else
+//             { response.render('pages/db', {results: result.rows} ); }
+//         });
+//     });
 // });
+
+
+
+app.get("/", function (req, res) {
+
+    res.send("Hello world");
+
+    // res.render("index", {})
+});
 
 app.get("/sign-up", function (req, res) {
 
